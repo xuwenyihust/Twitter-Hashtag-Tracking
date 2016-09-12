b5d50a12773e52ed737a62d64e794b6b59d3e640
xuwenyihust <1016635786@qq.com>
2016-09-12 14:17:42 -0700

    Some updates.

--- stream.py
+++ stream.py
@@ -6,8 +6,30 @@ import socket
 import json
 import os
 
-def api_access(self):
+def api_access():
 	config = {}
 	with open('config.json') as f:
-		config.update(son.load(f))
+		config.update(json.load(f))
+
+	return config
+
+#def send_data():
+
+
+
+if __name__ == '__main__':
+	# Get the Twitter API keys
+	config = api_access()
+	asecret = config['asecret']
+	atoken = config['atoken']
+	csecret = config['csecret']
+	ckey = config['ckey']
+
+	# Create a socket object
+	s = socket.socket()
+	# Get local machine name
+	hostname = socket.gethostname()
+	print('>>> Host Name:\t', hostname)
+
+
 
