0a032ba5fbb70836b25ed58b22abbea83945fad0
xuwenyihust <1016635786@qq.com>
2016-09-17 21:16:26 -0700

    Some updates.

---
+++ web/dashboard.py
@@ -0,0 +1,12 @@
+# Dashboard 
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
