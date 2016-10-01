ee312310452ada7f5ffdadaacdccea0331e838cd
xuwenyihust <1016635786@qq.com>
2016-10-01 16:25:49 -0700

    Some updates.

--- web/static/count.js
+++ web/static/count.js
@@ -2,29 +2,29 @@
 //
 // Counts Chart
 //
-var margin = {top: 20, right: 100, bottom: 70, left: 60},
-    width = 400 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+var count_margin = {top: 20, right: 100, bottom: 70, left: 60},
+    count_width = 1000 - count_margin.left - count_margin.right,
+    count_height = 300 - count_margin.top - count_margin.bottom;
 
 // set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-var y = d3.scale.linear().range([height, 0]);
+var count_x = d3.scale.ordinal().rangeRoundBands([0, count_width], .05);
+var count_y = d3.scale.linear().range([count_height, 0]);
 // define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
+var count_xAxis = d3.svg.axis()
+    .scale(count_x)
     .orient("bottom")
-var yAxis = d3.svg.axis()
-    .scale(y)
+var count_yAxis = d3.svg.axis()
+    .scale(count_y)
     .orient("left")
     .ticks(10);
 
 // add the SVG element
 var count = d3.select("#count").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", count_width + count_margin.left + count_margin.right)
+    .attr("height", count_height + count_margin.top + count_margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + count_margin.left + "," + count_margin.top + ")");
 
 // load the data
 d3.json("/data/counts", function(error, data) {
@@ -33,14 +33,14 @@ d3.json("/data/counts", function(error, data) {
         d.Count = d.Count;
     });
   // scale the range of the data
-  x.domain(data.map(function(d) { return d.Time; }));
+  count_x.domain(data.map(function(d) { return d.Time; }));
   //x.domain([0, d3.max(data, function(d) { return d.Time; })]);
-  y.domain([0, d3.max(data, function(d) { return d.Count; })]);
+  count_y.domain([0, d3.max(data, function(d) { return d.Count; })]);
   // add axis
   count.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + count_height + ")")
+      .call(count_xAxis)
     .selectAll("text")
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
@@ -48,7 +48,7 @@ d3.json("/data/counts", function(error, data) {
       .attr("transform", "rotate(-90)" );
   count.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(count_yAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 5)
@@ -57,8 +57,8 @@ d3.json("/data/counts", function(error, data) {
       .text("Count");
 
     var line = d3.svg.line()
-            .x(function(d) { return 30+x(d.Time); })
-            .y(function(d) { return y(d.Count);});
+            .x(function(d) { return 40+count_x(d.Time); })
+            .y(function(d) { return count_y(d.Count);});
 
 
   count.selectAll("path")
